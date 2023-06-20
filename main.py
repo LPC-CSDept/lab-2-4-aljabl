c14bb1e1c815b4c380182d31ba973951adf1db47
aljabl <alexandrablais@icloud.com>
2023-06-19 20:18:53 -0700

    lab4

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def main():
 
+    #assign variables
     original_str = 'Python Programming'
-    ##################################################
-    # Comlete your code here
-    ##################################################
 
+    #extract 'Python' from original string with index slicing
+    sub1 = original_str[:6]
+
+    #extract 'Programming' from original string with index slicing
+    sub2 = original_str[7:18]
+
+    #merge two substrings and save it to variable "merged_str"
+    merged_str = sub2 + ' ' + sub1
+    
     print(sub2)
     print(sub1)
     print(merged_str)
